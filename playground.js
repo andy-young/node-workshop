7983935569da2ab3acda59adab2207d9171cca17
Andy Young <ayscript@gmail.com>
2019-01-15 11:19:06 -0700

    Practice using modules.export & require

--- playground.js
+++
@@ -1,5 +0,0 @@
-console.log("'__dirname' gives the directory path:", __dirname);
-
-console.log("'process' gives info about the env the app is running in:", process);
-
-console.log("'global' is like the window object:", global);
